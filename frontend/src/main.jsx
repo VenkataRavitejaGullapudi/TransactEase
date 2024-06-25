d10008b269711b89a181873ca779f73150d5d8aa
Venkata Raviteja Gullapudi <venkataravitejagullapudi@gmail.com>
2024-06-25 10:00:39 +0530

    feat: init frontend setup + react router setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
-React from 'react'
-ReactDOM from 'react-dom/client'
-App from './App.jsx'
-'./index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
